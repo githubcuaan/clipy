d5f5f35c55084a69678af46911b4fcc5adcb093e
dnhfan <dinhan.0321@gmail.com>
2026-05-28 17:02:59 +0700

    rm unuse var

--- src/autopaste.sh
+++ src/autopaste.sh
@@ -5,7 +5,5 @@ auto_paste() {
     return 0
   fi
 
-  local active_class=$(hyprctl activewindow -j | grep -o '"class": "[^"]*"' | cut -d'"' -f4)
-
   hyprctl dispatch exec "sleep $CLIPY_AUTOPASTE_DELAY && wtype -M ctrl -M shift v -m shift -m ctrl"
 }
